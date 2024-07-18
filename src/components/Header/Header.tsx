d4e2abd1f8034adce0415c8b0be5c8039c0d9da6
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-07-18 23:48:54 +0900

    Feat: 헤더 컴포넌트 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,21 @@
-const Header = () => {
-  return <header>header</header>;
+interface HeaderProps {
+  leftUI?: React.ReactNode;
+  rightUI?: React.ReactNode;
+  title?: string;
+}
+
+const Header = ({ leftUI, rightUI, title }: HeaderProps) => {
+  return (
+    <header className="w-full h-[46px] flex items-center justify-between">
+      <div className="flex justify-center items-center min-w-[64px]">
+        {leftUI}
+      </div>
+      <h1 className="text-hl font-bold">{title}</h1>
+      <div className="flex justify-center items-center min-w-[64px]">
+        {rightUI}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
