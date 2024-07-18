2c4a64c4281e78db54fb3462fadf57be1e423ae6
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-07-18 23:48:38 +0900

    Feat: 레이아웃에 header, headerOption Prop 추가

--- src/Layouts/BasicLayout.tsx
+++ src/Layouts/BasicLayout.tsx
@@ -1,10 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/components/Header/Header.tsx";
 
-const BasicLayout = () => {
+interface BasicLayoutProps {
+  withHeader?: boolean;
+  withHeaderOptions?: {
+    leftUI?: React.ReactNode;
+    rightUI?: React.ReactNode;
+    title?: string;
+  };
+}
+
+const BasicLayout = ({ withHeader, withHeaderOptions }: BasicLayoutProps) => {
   return (
     <main className={"h-dvh w-[390px] mx-auto bg-white dark:bg-black"}>
-      <Header />
+      {withHeader && <Header {...withHeaderOptions} />}
       <Outlet />
     </main>
   );
