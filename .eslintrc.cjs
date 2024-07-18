00d131f88106422be8931b3ba9b442e9bb42bd89
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-07-18 23:15:15 +0900

    Chore: eslint-unused-import 플러그인 및 설정 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,22 @@
+require('eslint-plugin-unused-imports')
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'unused-imports'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', args: 'after-used'},
+      ],
   },
 }
