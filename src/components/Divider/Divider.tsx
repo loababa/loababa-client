b998a1b386bb6929065117634f5542ff364880e6
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-07-28 00:40:57 +0900

    Feat: 마이페이지 마크업 추가

---
+++ src/components/Divider/Divider.tsx
@@ -0,0 +1,34 @@
+import { ComponentPropsWithoutRef } from "react";
+import { cn } from "@/lib/utils.ts";
+
+interface DividerProps extends ComponentPropsWithoutRef<"div"> {
+  size?: 0 | 1 | 2 | 4;
+}
+
+const Divider = ({ size = 1, ...props }: DividerProps) => {
+  const borderSizeStyle = () => {
+    switch (size) {
+      case 0:
+        return "border-0";
+      case 1:
+        return "border";
+      case 2:
+        return "border-2";
+      case 4:
+        return "border-4";
+    }
+  };
+  const { className: propsClassName, ...rest } = props;
+  return (
+    <div
+      className={cn(
+        "w-full h-0.5 border-gray-800",
+        borderSizeStyle(),
+        propsClassName
+      )}
+      {...rest}
+    />
+  );
+};
+
+export default Divider;
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,7 +1,86 @@
+import { MockAvatar } from "@/components/icons";
+import { Badge } from "@/components/ui/badge.tsx";
+import { clsx } from "clsx";
+import Divider from "@/components/Divider/Divider.tsx";
+
 const MyPage = () => {
   return (
-    <div>
-      <h1>My Page</h1>
+    <div className="flex flex-col mb-[300px]">
+      <section className="flex flex-col gap-[10px] px-[20px] mb-[30px]">
+        <div className="flex gap-[8px]">
+          <MockAvatar
+            width={40}
+            height={40}
+          />
+          <div className="flex flex-col">
+            <span className="flex gap-[4px] items-center">
+              <span className="text-[12px]">로쌤만믿어</span>
+              <div className="text-c1 text-gray-300 py-[4px] px-[6px] bg-gray-800 rounded-[4px]">
+                Lv.1670+
+              </div>
+            </span>
+            <div className="flex gap-[4px] items-center">
+              <span className="flex gap-[4px] items-center">
+                <div className="w-[14px] h-[14px] rounded-full bg-gray-800" />
+                <span className="text-[12px] text-gray-500">직업1</span>
+              </span>
+              <span className="flex gap-[4px] items-center">
+                <div className="w-[14px] h-[14px] rounded-full bg-gray-800" />
+                <span className="text-[12px] text-gray-500">직업2</span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="flex gap-[4px] items-center">
+          <Badge
+            variant="outline"
+            className={clsx(
+              "w-fit min-h-[24px]",
+              "dark:bg-gray-900 dark:text-[10px] dark:text-gray-200 dark:font-normal dark:border-gray-800"
+            )}>
+            키워드글자수
+          </Badge>
+          <Badge
+            variant="outline"
+            className={clsx(
+              "w-fit min-h-[24px]",
+              "dark:bg-gray-900 dark:text-[10px] dark:text-gray-200 dark:font-normal dark:border-gray-800"
+            )}>
+            키워드글자수
+          </Badge>
+          <Badge
+            variant="outline"
+            className={clsx(
+              "w-fit min-h-[24px]",
+              "dark:bg-gray-900 dark:text-[10px] dark:text-gray-200 dark:font-normal dark:border-gray-800"
+            )}>
+            키워드글자수
+          </Badge>
+          <Badge
+            variant="outline"
+            className={clsx(
+              "w-fit min-h-[24px]",
+              "dark:bg-gray-900 dark:text-[10px] dark:text-gray-200 dark:font-normal dark:border-gray-800"
+            )}>
+            키워드글자수
+          </Badge>
+        </div>
+      </section>
+      <Divider
+        size={4}
+        className="mb-[15px]"
+      />
+      <section>
+        <div className="w-full flex items-center justify-start px-[20px] py-[16px]">
+          <span className="text-gray-400 font-normal">프로필 수정</span>
+        </div>
+        <div className="w-full flex items-center justify-start px-[20px] py-[16px]">
+          <span className="text-gray-400 font-normal">로아바바 소개</span>
+        </div>
+        <div className="w-full flex items-center justify-start px-[20px] py-[16px]">
+          <span className="text-red font-normal">회원탈퇴</span>
+        </div>
+      </section>
     </div>
   );
 };
