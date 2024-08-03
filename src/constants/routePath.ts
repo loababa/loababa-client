cee9d60b139b59d9098814b5ef877ea42a87d8f8
sscoderati <chungup5495@gmail.com>
2024-08-03 20:17:28 +0900

    Chore: 라우트 주소 수정

--- src/constants/routePath.ts
+++ src/constants/routePath.ts
@@ -1,10 +1,10 @@
 const ROUTE_PATH = {
   HOME: "/",
   LOGIN: "/login",
-  SIGNUP_TUTOR: "/signup/tutor",
-  SIGNUP_TUTEE: "/signup/tutee",
-  TUTOR_DETAIL: (id: string) => `/tutor/${id}`,
-  CONSULTING_REQUEST: (id: string) => `/tutor/${id}/consulting-request`,
+  SIGNUP_TUTOR: "/lossam/signup",
+  SIGNUP_TUTEE: "/mokoko/signup",
+  TUTOR_DETAIL: (id: string) => `/lossam/${id}`,
+  CONSULTING_REQUEST: (id: string) => `/lossam/${id}/consulting-request`,
   CONSULTING: "/consulting",
   CONSULTING_DETAIL: (id: string) => `/consulting/${id}`,
   MYPAGE: "/mypage"
