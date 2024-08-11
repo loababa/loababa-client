66ff34038a434d9b35f8286d0befc17cfdfcc2ad
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-08-11 12:28:54 +0900

    Style: Stepper 스타일 일부 수정

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -21,13 +21,13 @@ const Stepper = ({ stepInfo }: StepperProps) => {
   }, [currentStep]);
   return (
     <div className="w-full flex flex-col items-center px-[20px] pb-[30px]">
-      <div className="w-full flex justify-center items-center min-h-[65px] rounded-[8px] bg-gray-950 px-[5px] py-[16px]">
+      <div className="w-full flex justify-center items-center min-h-[65px] rounded-[8px] bg-gray-950 px-[20px] py-[16px]">
         {stepInfo.map((step, index) => (
           <div
-            className="flex items-center"
+            className={"flex w-full items-center"}
             key={`stepper-step-${index}`}>
             <div
-              className="flex flex-col justify-center items-center gap-[7px] cursor-pointer"
+              className="flex flex-col items-center gap-[7px] cursor-pointer w-full"
               onClick={() => setCurrentStep(index)}>
               <span
                 key={index}
@@ -37,12 +37,12 @@ const Stepper = ({ stepInfo }: StepperProps) => {
                 )}>
                 {step.label}
               </span>
-              <div className="flex items-center w-full">
+              <div className="flex items-center justify-start w-full">
                 {/* Step 내부의 왼쪽 라인 */}
                 <div
                   className={clsx(
                     "h-0.5 w-full border",
-                    index === 0 ? "border-transparent" : "border-gray-600"
+                    index === 0 ? "border-none" : "border-gray-600"
                   )}
                 />
                 <div
@@ -56,18 +56,12 @@ const Stepper = ({ stepInfo }: StepperProps) => {
                   className={clsx(
                     "h-0.5 w-full border",
                     index === stepInfo.length - 1
-                      ? "border-transparent"
+                      ? "border-none"
                       : "border-gray-600"
                   )}
                 />
               </div>
             </div>
-            <div
-              className={clsx(
-                "flex items-center h-0.5 min-w-[30px] max-w-[40px] bg-gray-600 mt-[23px]",
-                index === stepInfo.length - 1 ? "hidden" : "block"
-              )}
-            />
           </div>
         ))}
       </div>
