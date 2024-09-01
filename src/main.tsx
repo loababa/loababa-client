f96f9fdd381aa4576f96c67d19624a6b4300587e
sscoderati <chungup5495@gmail.com>
2024-09-01 13:53:48 +0900

    Chore: QueryDevtools 추가

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import router from "@/router.tsx";
 import { ThemeProvider } from "@/components/Provider/ThemeProvider.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "@/lib/queryClient.ts";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
@@ -13,5 +14,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       storageKey="vite-ui-theme">
       <RouterProvider router={router} />
     </ThemeProvider>
+    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 );
