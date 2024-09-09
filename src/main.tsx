9038f5592c00bd310c13794df86189c7c2a09400
sscoderati <chungup5495@gmail.com>
2024-09-09 23:27:58 +0900

    Chore: sonner 추가

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@/components/Provider/ThemeProvider.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "@/lib/queryClient.ts";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "@/components/ui/sonner.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
@@ -15,5 +16,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <RouterProvider router={router} />
     </ThemeProvider>
     <ReactQueryDevtools initialIsOpen={false} />
+    <Toaster
+      richColors
+      position="top-center"
+    />
   </QueryClientProvider>
 );
