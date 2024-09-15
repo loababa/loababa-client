6fad5922c96a4df723c13e0741c181dc674e044e
sscoderati <chungup5495@gmail.com>
2024-09-16 06:52:38 +0900

    Chore: 개발 / 배포환경에 따라 카카오 oauth redirect uri 분리

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -7,6 +7,10 @@ const LoginPage = () => {
   const location = useLocation();
 
   const lossamKey = location.search.split("=")[1];
+
+  const loginRedirectUri = import.meta.env.PROD
+    ? "https://loababa.com/lossam/signup"
+    : "http://localhost:5173/lossam/signup";
   return (
     <section className="px-[20px]">
       <div className="flex flex-col items-center justify-center gap-[4px] my-[180px]">
@@ -23,7 +27,7 @@ const LoginPage = () => {
         <h1 className="text-[16px] font-semibold">간편 로그인</h1>
         <div className="flex flex-col gap-[8px] w-full mt-[20px]">
           <Link
-            to={`https://kauth.kakao.com/oauth/authorize?client_id=${import.meta.env.VITE_KAKAO_CLIENT_ID}&redirect_uri=http://localhost:5173/lossam/signup&response_type=code&state=${lossamKey ?? ""}`}
+            to={`https://kauth.kakao.com/oauth/authorize?client_id=${import.meta.env.VITE_KAKAO_CLIENT_ID}&redirect_uri=${loginRedirectUri}&response_type=code&state=${lossamKey ?? ""}`}
             rel="noreferrer">
             <Button
               className={clsx(
